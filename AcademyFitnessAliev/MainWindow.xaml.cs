8b43b17b8e38202e5578b866db37722ba43ed25d
HP <sultanagamirzaev@mail.ru>
2024-02-09 13:02:52 +0300

    Добавьте файлы проекта.

---
+++ AcademyFitnessAliev/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AcademyFitnessAliev
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Academy_fitnessEntities context;
+        public MainWindow()
+        {
+            InitializeComponent();
+            context = new Academy_fitnessEntities();
+            CmbSelectTrainer.ItemsSource = context.Trainers.ToList();
+            RegistrationGrid.ItemsSource = context.CourseRegistrations.ToList();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            var newRegistration = new CourseRegistration();
+            context.CourseRegistrations.Add(newRegistration);
+            var win = new RegistrationCourseWindow(context, newRegistration);
+            win.ShowDialog();
+            RegistrationGrid.ItemsSource = context.CourseRegistrations.ToList();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var row = RegistrationGrid.SelectedItem as CourseRegistration;
+            if (row == null)
+            {
+                MessageBox.Show("Строка не выбрана");
+                return;
+            }
+
+            MessageBoxResult result = MessageBox.Show("Вы уверены?", "Вопрос", MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                context.CourseRegistrations.Remove(row);
+                context.SaveChanges();
+                RegistrationGrid.ItemsSource = context.CourseRegistrations.ToList();
+            }
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button EditButton = sender as Button;
+            var currentRegistration = EditButton.DataContext as CourseRegistration;
+            var win = new RegistrationCourseWindow(context, currentRegistration);
+            win.ShowDialog();
+        }
+
+        private void FiltrButton_Click(object sender, RoutedEventArgs e)
+        {
+            RegistrationGrid.ItemsSource = context.CourseRegistrations.Where(x => x.IsDone == true).ToList();
+            MessageBoxResult result = MessageBox.Show("Вернуть все записи?", "Вопрос", MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                RegistrationGrid.ItemsSource = context.CourseRegistrations.ToList();
+            }
+        }
+        private void CmdSelectTrainer_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (CmbSelectTrainer.SelectedItem == null)
+                return;
+
+            var currentTrainer = (Trainer)CmbSelectTrainer.SelectedItem;
+            List<CourseRegistration> ListTrainer = context.CourseRegistrations.ToList();
+            RegistrationGrid.ItemsSource = ListTrainer.Where(x => x.Trainer == currentTrainer).ToList();
+        }
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            RegistrationGrid.ItemsSource = context.CourseRegistrations.ToList();
+        }
+    }
+}
---
+++ AcademyFitnessAliev/RegistrationCourseWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AcademyFitnessAliev
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationCourseWindow.xaml
+    /// </summary>
+    public partial class RegistrationCourseWindow : Window
+    {
+        Academy_fitnessEntities context;
+        public RegistrationCourseWindow(Academy_fitnessEntities context, CourseRegistration currentRegistration)
+        {
+            InitializeComponent();
+            this.context = context;
+            CmbCourse.ItemsSource = context.Courses.ToList();
+            CmbTrainer.ItemsSource = context.Trainers.ToList();
+
+            this.DataContext = currentRegistration;
+        }
+        
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (CheckRegistration())
+            {
+                context.SaveChanges();
+                this.Close();
+            }
+        }
+
+        private bool CheckRegistration()
+        {
+            var reg = this.DataContext as CourseRegistration;
+            if (reg.Trainer == null)
+            {
+                MessageBox.Show("Тренер не выбран");
+                return false;
+            }
+            if (reg.Course == null)
+            {
+                MessageBox.Show("Курсы не выбраны");
+                return false;
+            }
+            if (reg.CreateDate == null)
+            {
+                MessageBox.Show("Дата не выбрана");
+                return false;
+            }
+            if (!int.TryParse(TxtTotalPoint.Text, out int result))
+            {
+                MessageBox.Show("Неверные данные");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
